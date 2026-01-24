d97298511d72433ba23fa26b8428f31409ed230e
Michael Turbe <mturbe@gmail.com>
2026-01-24 00:30:37 -0500

    thing

--- parallax-protocol-angular/src/app/app.routes.ts
+++ parallax-protocol-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CharacterSheetComponent } from '../character-sheet/character-sheet.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sheet',
+    component: CharacterSheetComponent
+  }
+];
---
+++ parallax-protocol-angular/src/character-sheet/epithet/epithet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epithet',
+  imports: [],
+  templateUrl: './epithet.component.html',
+  styleUrl: './epithet.component.scss'
+})
+export class EpithetComponent {
+
+}
