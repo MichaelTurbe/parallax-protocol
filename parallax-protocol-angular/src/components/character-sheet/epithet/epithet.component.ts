4ad13709c7d556d1d3c158e640b11c85817a909c
Michael Turbe <mturbe@gmail.com>
2026-01-24 23:44:23 -0500

    WIP MESS

--- parallax-protocol-angular/src/components/character-sheet/epithet/epithet.component.ts
+++ parallax-protocol-angular/src/components/character-sheet/epithet/epithet.component.ts
@@ -1,5 +1,5 @@
 import { Component, Signal } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+// import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import {form, FormField, required, email} from '@angular/forms/signals';
 
 
@@ -10,7 +10,7 @@ import {form, FormField, required, email} from '@angular/forms/signals';
   styleUrl: './epithet.component.scss'
 })
 export class EpithetComponent {
-  nameControl = new FormControl('');
+  // nameControl = new FormControl('');
   // nameSignal: Signal<any>;
 
 }
